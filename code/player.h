9988c49dbd534deb38fea646269b792b5e9a4b96
Anlor <chenxiex@outlook.com>
2023-07-13 20:15:08 +0800

    4435

--- code/player.h
+++ code/player.h
@@ -233,36 +233,7 @@ static void enemy_dist(Player *player, int step[MAXN][MAXN])
         }
     }
 }
-/*
-static int put(Player *tmp)
-{
-    int score = 0;
-    if (tmp->mat[tmp->your_posx][tmp->your_posy] == 'o' || tmp->mat[tmp->your_posx][tmp->your_posy] == 'O')
-    {
-        score += 10;
-        tmp->mat[tmp->your_posx][tmp->your_posy] = '.';
-    }
-    if (tmp->your_status > 0)
-    {
-        if (tmp->your_posx == tmp->ghost_posx[0] && tmp->your_posy == tmp->ghost_posy[0])
-        {
-            score += 200;
-            tmp->ghost_posx[0] = ghostx[0], tmp->ghost_posy[0] = tmp->ghost_posy[0];
-        }
-        if (tmp->your_posx == tmp->ghost_posx[1] && tmp->your_posy == tmp->ghost_posy[1])
-        {
-            score += 200;
-            tmp->ghost_posx[1] = ghostx[1], tmp->ghost_posy[1] = tmp->ghost_posy[1];
-        }
-        if (tmp->your_posx == tmp->opponent_posx && tmp->your_posy == tmp->opponent_posy && tmp->opponent_status == 0)
-        {
-            score += tmp->opponent_score / 2;
-            tmp->opponent_status = -1;
-        }
-    }
-    return score;
-}
-*/
+
 static int calcscore(Player *tmp)
 {
     int score = 0;
@@ -374,78 +345,7 @@ static bool unsafe(Player *now)
     }
     return false;
 }
-/*
-static Point run_away(Player *now)
-{
-    struct qNode
-    {
-        Point pos;
-        int step;
-        int direction;
-    };
-    std::queue<qNode> q;
-    bool flag[MAXN][MAXN];
-    memset(flag, false, sizeof(flag));
-    q.push({now->your_posx, now->your_posy, 0, -1});
-    flag[now->your_posx][now->your_posy] = true;
-    int nd = -1, ndx = 3, mind = 0;
-    while (!q.empty())
-    {
-        qNode tmp = q.front();
-        q.pop();
-        for (int i = 0; i < 4; i++)
-        {
-            qNode next = tmp;
-            next.pos.X += MOV[i].X, next.pos.Y += MOV[i].Y;
-            next.step++;
-            if (next.direction == -1)
-                next.direction = i;
-            if (check(next.pos, now) && ed[next.pos.X][next.pos.Y] > next.step && !flag[next.pos.X][next.pos.Y])
-            {
-                if (choice[next.pos.X][next.pos.Y] >= ndx && next.step < ed[next.pos.X][next.pos.Y])
-                {
-                    ndx = choice[next.pos.X][next.pos.Y];
-                    if (ed[next.pos.X][next.pos.Y] - next.step >= mind)
-                    {
-                        mind = ed[next.pos.X][next.pos.Y] - next.step;
-                        nd = next.direction;
-                    }
-                }
-                if (now->mat[next.pos.X][next.pos.Y] == 'O' && ed[next.pos.X][next.pos.Y] < 19)
-                {
-                    return {now->your_posx + MOV[next.direction].X, now->your_posy + MOV[next.direction].Y};
-                }
-                q.push(next);
-                flag[next.pos.X][next.pos.Y] = true;
-            }
-        }
-    }
-    if (nd != -1)
-    {
-        return {now->your_posx + MOV[nd].X, now->your_posy + MOV[nd].Y};
-    }
-    int maxd = -1, mini = -1;
-    for (int i = 0; i < 4; i++)
-    {
-        Point next = {now->your_posx + MOV[i].X, now->your_posy + MOV[i].Y};
-        if (!check(next, now))
-            continue;
-        if (ed[next.X][next.Y] > maxd)
-        {
-            maxd = ed[next.X][next.Y];
-            mini = i;
-        }
-    }
-    if (maxd > ed[now->your_posx][now->your_posy])
-    {
-        return {now->your_posx + MOV[mini].X, now->your_posy + MOV[mini].Y};
-    }
-    else
-    {
-        return {now->your_posx, now->your_posy};
-    }
-}
-*/
+
 static Point hunt(Player *now)
 {
     int mind = 0x3f3f3f3f, mini = -1;
@@ -462,47 +362,7 @@ static Point hunt(Player *now)
     }
     return {now->your_posx + MOV[mini].X, now->your_posy + MOV[mini].Y};
 }
-/*
-static struct Point get_star(struct Player *start)
-{
-    struct Point ret = {start->your_posx, start->your_posy};
-    int maxx = 0, maxi = -1;
-    for (int i = 0; i < 4; i++)
-    {
-        Player tmp = *start;
-        tmp.your_posx += MOV[i].X;
-        tmp.your_posy += MOV[i].Y;
-        if (!check({tmp.your_posx, tmp.your_posy}, start))
-            continue;
-        int h = calcscore(&tmp) + choice[tmp.your_posx][tmp.your_posy];
-        if (h > maxx)
-        {
-            maxx = h;
-            maxi = i;
-        }
-        else
-        {
-            if (maxx < 10 && walked[tmp.your_posx][tmp.your_posx] == false)
-            {
-                if (maxi == -1)
-                {
-                    maxx = h;
-                    maxi = i;
-                }
-                else if (walked[start->your_posx + MOV[maxi].X][start->your_posy + MOV[maxi].Y])
-                {
-                    maxx = h;
-                    maxi = i;
-                }
-            }
-        }
-    }
-    int x1 = start->your_posx + MOV[maxi].X, y1 = start->your_posy + MOV[maxi].Y;
-    walked[x1][y1] = true;
-    ret = {start->your_posx + MOV[maxi].X, start->your_posy + MOV[maxi].Y};
-    return ret;
-}
-*/
+
 static Point get_star(Player *now)
 {
     struct qNode
